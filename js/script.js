7c8930e746ce2b9569679fb96cda8f639b4726ce
Ismael Nasciemnto <117104139+ismaelvieira12@users.noreply.github.com>
2024-06-23 12:30:49 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -98,40 +98,41 @@ const ctx = document.getElementById('chart');
     }]
   }
 
-function meses(jan23, abr23, mai23, jun23, jul23, ago23, set23, out23, nov23, dez23){
+const teste = document.querySelector('#teste');
+const spinIn = document.querySelector('#spinIn');
 
-  valorAnual = jan23.api[20].TOTAL +
-  jan23.api[0].TOTAL +
-  jan23.api[0].TOTAL +
-  abr23.api[26].TOTAL +
-  mai23.api[28].TOTAL +
-  jun23.api[31].TOTAL +
-  jul23.api[35].TOTAL +
-  ago23.api[36].TOTAL +
-  set23.api[38].TOTAL +
-  out23.api[32].TOTAL + 
-  nov23.api[74].TOTAL +  
-  nov23.api[37].TOTAL + 
-  nov23.api[37].TOTAL +
-  dez23.api[15].TOTAL +  
-  dez23.api[36].TOTAL + 
-  dez23.api[69].TOTAL ;
+function meses(ano2022){
+
+  // jan23.api[0].TOTAL +
+  // jan23.api[0].TOTAL +
+  // abr23.api[26].TOTAL +
+  // mai23.api[28].TOTAL +
+  // jun23.api[31].TOTAL +
+  // jul23.api[35].TOTAL +
+  // ago23.api[36].TOTAL +
+  // set23.api[38].TOTAL +
+  // out23.api[32].TOTAL + 
+  // nov23.api[74].TOTAL +  
+  // nov23.api[37].TOTAL + 
+  // nov23.api[37].TOTAL +
+  // dez23.api[15].TOTAL +  
+  // dez23.api[36].TOTAL + 
+  // dez23.api[69].TOTAL ;
+  console.log(ano2022);
   
-  onload(valorAnual);
+  // onload(valorAnual);
 
 
-  console.log(jan23);
-  console.log(abr23);
-  console.log(mai23);
-  console.log(jun23);
-  console.log(jul23);
-  console.log(ago23);
-  console.log(set23);
-  console.log(out23);
-  console.log(nov23);
-  console.log(dez23);
-  let novembro = nov23.api[74].TOTAL +  nov23.api[37].TOTAL + nov23.api[37].TOTAL;
-  let dezembro = dez23.api[15].TOTAL +  dez23.api[36].TOTAL + dez23.api[69].TOTAL;
+  // console.log(jan23);
+  // console.log(abr23);
+  // console.log(mai23);
+  // console.log(jun23);
+  // console.log(jul23);
+  // console.log(ago23);
+  // console.log(set23);
+  // console.log(out23);
+  // console.log(nov23);
+  
   const graphicBar = document.getElementById('g1');
     new Chart(graphicBar, {
       type: 'bar',
@@ -140,18 +141,18 @@ function meses(jan23, abr23, mai23, jun23, jul23, ago23, set23, out23, nov23, de
         datasets: [{
           label: 'R$',
           data: {
-            'Jan 2023': jan23.api[20].TOTAL, 
-            'Fer 2023': jan23.api[0].TOTAL ,
-            'Mar 2023': jan23.api[0].TOTAL ,
-            'Abr 2023': abr23.api[26].TOTAL,
-            'Mai 2023': mai23.api[28].TOTAL,
-            'Jun 2023': jun23.api[31].TOTAL,
-            'Jul 2023': jul23.api[35].TOTAL,
-            'Ago 2023': ago23.api[36].TOTAL,
-            'Set 2023': set23.api[38].TOTAL,
-            'Out 2023': out23.api[32].TOTAL,
-            'Nov 2023': novembro,
-            'Dez 2023': dezembro,
+            'Jan 2022': ano2022.api[0].VALOR , 
+            'Fer 2022': ano2022.api[1].VALOR ,
+            'Mar 2022': ano2022.api[2].VALOR ,
+            'Abr 2022': ano2022.api[3].VALOR ,
+            'Mai 2022': ano2022.api[4].VALOR ,
+            'Jun 2022': ano2022.api[5].VALOR ,
+            'Jul 2022': ano2022.api[6].VALOR ,
+            'Ago 2022': ano2022.api[7].VALOR ,
+            'Set 2022': ano2022.api[8].VALOR ,
+            'Out 2022': ano2022.api[9].VALOR ,
+            'Nov 2022': ano2022.api[10].VALOR ,
+            'Dez 2022': ano2022.api[11].VALOR 
 
          } ,
           borderWidth: 1,
@@ -175,26 +176,24 @@ function meses(jan23, abr23, mai23, jun23, jul23, ago23, set23, out23, nov23, de
 
 
 async function api(){
-  const jan23 = await fetch('https://script.google.com/macros/s/AKfycbyv9-o9vDKlffGR_y0HQRVyk5HwmE0Bc15xk0MShYTp8AwQMcg2xiQ2C9ez4OlKklpo/exec').then(response => response.json());
-  const abr23 = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=VtmP1AIZ6RF4lTLDjwWRVtnXirTZ1e9vI8660EsjO7hgeiNTOdUmDiy92I-AVLf0A6kE4RuFi4M3T4Jqati4tcqM-yuI1QMgm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnC6zHBlGXRb4_Lpawi71PK31NEniKNRBBIOr4pjt_P19fgoUwW5nsEt2KsWTK97MvCElphgE6Zc-Z-0JfF49MDHshTl8x83JkA&lib=M8G5hm_VlBnB5nuEPbx8Vg6frgnVbBec2').then(response => response.json());
-  const mai23 = await fetch('https://script.google.com/macros/s/AKfycbzXM_dRxEZ0fmCNQTWQb4rk9HkH6mdYRMgJoA_QzvOzYq9IozE6HXEZEXRa888CTC0N/exec').then(response => response.json());
-  const jun23 = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=DNXV5uYEr6AwkD7Q6BtSPe2FFaEmGfg6_SCZS23wEncKZI_gLcxyDbYZXL4eQ5NFqiJ0YjY3OEPNFLfn4QxZzUh4QZdAonEjm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLjdpmtY8sWDmJXzSpJ8CdK-h6XOUNGxIl1v3K2RZeP2TUZjwPrB8_bXeDA24R2187wYnaKOPZPbO-ki0ZRdLxsW5mHJiC7Yag&lib=M8G5hm_VlBnB5nuEPbx8Vg6frgnVbBec2').then(response => response.json());
-  const jul23 = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=6CSmAv97wf1AutV8Adfdy0-d1y4mf1rbiMkRfD2oBZVWCj1NKVwu3hy-pIDJgZS1uddAOpm2l-B5oV92jWRZbtdtwfPQqTd0m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnD_rqQViQvLjxyOdpskk159XMIRVpEYHsjfC1wYLx10Bsjz1j2A8EjRSofX8rLPpgv6joDzRjz6kvQo6YJTGd6ZV8BCno9Aazw&lib=M8G5hm_VlBnB5nuEPbx8Vg6frgnVbBec2').then(response => response.json());
-  const ago23 = await fetch('https://script.google.com/macros/s/AKfycbw7faf45Oz4nDse85Ik4u4WmQTe5FYsIpHe1zkoa95-Cbs6C1rsQIPzh7nHDLxCVnak/exec').then(response => response.json());
-  const set23 = await fetch('https://script.google.com/macros/s/AKfycbxqlrxTrHocWMdPdQs5dHd9VdsJAtqE6oU8RsPIQJz43eq6w20X9QXpa3zOetgnDn26/exec').then(response => response.json());
-  const out23 = await fetch('https://script.google.com/macros/s/AKfycbxIJdZ2BXz6fW559XiitN8jrmYD3PgxdwGv0W8p5Z54KF4GYUD_Hbyxz6lsxDGmDw2e/exec').then(response => response.json());
-  const nov23 = await fetch('https://script.google.com/macros/s/AKfycbzYYfVTUofN5tXnsQRyfbHUWLgitXouRTL0J4O0XKGpP72kZX889RaenS5_dV-s4Hea/exec').then(response => response.json());
-  const dez23 = await fetch('https://script.google.com/macros/s/AKfycbxzABAvDqLS_fYb3FF92y6TK8YgmmGxXxbwqEF70gTKYWNhhhFyXsxFysKxx6uX2yvA/exec').then(response => response.json());
+  // const jan23 = await fetch('https://script.google.com/macros/s/AKfycbyv9-o9vDKlffGR_y0HQRVyk5HwmE0Bc15xk0MShYTp8AwQMcg2xiQ2C9ez4OlKklpo/exec').then(response => response.json());
+  // const abr23 = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=VtmP1AIZ6RF4lTLDjwWRVtnXirTZ1e9vI8660EsjO7hgeiNTOdUmDiy92I-AVLf0A6kE4RuFi4M3T4Jqati4tcqM-yuI1QMgm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnC6zHBlGXRb4_Lpawi71PK31NEniKNRBBIOr4pjt_P19fgoUwW5nsEt2KsWTK97MvCElphgE6Zc-Z-0JfF49MDHshTl8x83JkA&lib=M8G5hm_VlBnB5nuEPbx8Vg6frgnVbBec2').then(response => response.json());
+  // const mai23 = await fetch('https://script.google.com/macros/s/AKfycbzXM_dRxEZ0fmCNQTWQb4rk9HkH6mdYRMgJoA_QzvOzYq9IozE6HXEZEXRa888CTC0N/exec').then(response => response.json());
+  // const jun23 = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=DNXV5uYEr6AwkD7Q6BtSPe2FFaEmGfg6_SCZS23wEncKZI_gLcxyDbYZXL4eQ5NFqiJ0YjY3OEPNFLfn4QxZzUh4QZdAonEjm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLjdpmtY8sWDmJXzSpJ8CdK-h6XOUNGxIl1v3K2RZeP2TUZjwPrB8_bXeDA24R2187wYnaKOPZPbO-ki0ZRdLxsW5mHJiC7Yag&lib=M8G5hm_VlBnB5nuEPbx8Vg6frgnVbBec2').then(response => response.json());
+  // const jul23 = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=6CSmAv97wf1AutV8Adfdy0-d1y4mf1rbiMkRfD2oBZVWCj1NKVwu3hy-pIDJgZS1uddAOpm2l-B5oV92jWRZbtdtwfPQqTd0m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnD_rqQViQvLjxyOdpskk159XMIRVpEYHsjfC1wYLx10Bsjz1j2A8EjRSofX8rLPpgv6joDzRjz6kvQo6YJTGd6ZV8BCno9Aazw&lib=M8G5hm_VlBnB5nuEPbx8Vg6frgnVbBec2').then(response => response.json());
+  // const ago23 = await fetch('https://script.google.com/macros/s/AKfycbw7faf45Oz4nDse85Ik4u4WmQTe5FYsIpHe1zkoa95-Cbs6C1rsQIPzh7nHDLxCVnak/exec').then(response => response.json());
+  // const set23 = await fetch('https://script.google.com/macros/s/AKfycbxqlrxTrHocWMdPdQs5dHd9VdsJAtqE6oU8RsPIQJz43eq6w20X9QXpa3zOetgnDn26/exec').then(response => response.json());
+  // const out23 = await fetch('https://script.google.com/macros/s/AKfycbxIJdZ2BXz6fW559XiitN8jrmYD3PgxdwGv0W8p5Z54KF4GYUD_Hbyxz6lsxDGmDw2e/exec').then(response => response.json());
+  // const nov23 = await fetch('https://script.google.com/macros/s/AKfycbzYYfVTUofN5tXnsQRyfbHUWLgitXouRTL0J4O0XKGpP72kZX889RaenS5_dV-s4Hea/exec').then(response => response.json());
+  const ano2022 = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=8sHwDIHnYRVCs5BdbUBmr9nOgpVuyWAj8WMkmdUwi6P5x7mmNvMFHO4Omrf6StvnlT_kd5ysu5ahERnSfVIUQnPSJCpsOKcVm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFVXWXR0nSELR97zGgQBms-7dZnT2diY5rajuaka_Z0rKkB6hHYVkl7PctLAiJohTZfdx3kKMqce8bLhRpGbczYtqRaF3dzGGA&lib=M8G5hm_VlBnB5nuEPbx8Vg6frgnVbBec2').then(response => response.json());
 
-  meses(jan23, abr23, mai23, jun23, jul23, ago23, set23, out23, nov23, dez23);
- 
-  
+  meses(ano2022);
 }
 
 window.onload = function(valorAnual){
   const valorTotal = document.querySelector('#valueTotal');
   const numberAnual = parseFloat(valorAnual);
-  valorTotal.innerText = `$ ${numberAnual.toFixed(2)}`;
+  valorTotal.innerText = `${numberAnual.toFixed(2)}`;
 }
 
 api();
