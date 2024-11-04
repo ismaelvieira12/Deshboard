58c065bd7c6d0cecc1e846d33849da89beab7c12
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-11-04 14:52:40 -0300

    fazendo os testes no grafico mensal

--- js/grafico_mensal.js
+++ js/grafico_mensal.js
@@ -1,266 +1,125 @@
-
-
-
-
-// Configuring the style of the chart
-function testando(meses2024){
-
-        Highcharts.setOptions({
-            lang: {
-                rangeSelectorZoom: ''
-            },
-        
-            chart: {
-                backgroundColor: 'transparent',
-                zooming: {
-                    resetButton: {
-                        theme: {
-                            fill: '#8870fe6a',
-                            stroke: 'none',
-                            style: {
-                                color: '#ffffff'
-                            },
-                            r: 8,
-                            states: {
-                                hover: {
-                                    style: {
-                                        color: '#79a6ff'
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-        
-            title: {
-                style: {
-                    color: '#ABA9D9',
-                    fontSize: '2em'
-                }
-            },
-        
-            xAxis: {
-                labels: {
-                    style: {
-                        color: '#79a6ff'
-                    }
-                }
-            },
-        
-            yAxis: {
-                labels: {
-                    style: {
-                        color: '#79a6ff'
-                    }
-                }
-            },
-        
-            navigator: {
-                series: {
-                    fillColor: {
-                        linearGradient: [0, 0, 0, 40],
-                        stops: [
-                            [0, '#79a6ff'],
-                            [1, '#000000']
-                        ]
-                    }
-                },
-                xAxis: {
-                    labels: {
+// Configurando o estilo do gráfico
+function testando(diasDoMes) {
+    Highcharts.setOptions({
+        lang: {
+            rangeSelectorZoom: ''
+        },
+    
+        chart: {
+            backgroundColor: 'transparent',
+            zooming: {
+                resetButton: {
+                    theme: {
+                        fill: '#8870fe6a',
+                        stroke: 'none',
                         style: {
-                            color: '#79a6ff',
-                            opacity: 1,
-                            textOutline: '#79a6ff'
-                        }
-                    }
-                },
-                maskFill: '#79a6ff',
-                handles: {
-                    backgroundColor: '#79a6ff',
-                    borderRadius: '50%',
-                    width: 20,
-                    height: 20
-                }
-            },
-        
-            exporting: {
-                buttons: {
-                    contextButton: {
-                        symbolStroke: '#d9d7d7',
-                        theme: {
-                            fill: '#52459ea8',
-                            states: {
-                                hover: {
-                                    fill: '#79a6ff' 
-                                },
-                                select: {
-                                    fill: '#52459e'
+                            color: '#ffffff'
+                        },
+                        r: 8,
+                        states: {
+                            hover: {
+                                style: {
+                                    color: '#79a6ff'
                                 }
                             }
                         }
                     }
-                }
-            },
-        
-            scrollbar: {
-                barBackgroundColor: '#79a6ff',
-                trackBorderColor: '#52459e'
-            },
-        
-            rangeSelector: {
-                buttonTheme: {
-                    fill: 'none',
-                    stroke: 'none',
-                    'stroke-width': 0,
-                    r: 8,
-                    style: {
-                        color: '#52459ea8',
-                        fontWeight: 'bold',
-                        fontSize: '1em'
-                    },
-                    states: {
-                        select: {
-                            fill: '#52459ea8',
-                            style: {
-                                color: '#ffffff'
-                            }
-                        },
-                        hover: {
-                            fill: 'none',
-                            stroke: '#52459ea8',
-                            'stroke-width': 2,
-                            style: {
-                                color: '#52459ea8'
-                            }
-                        }
-                    }
-                }
-            },
-        
-            plotOptions: {
-                area: {
-                    threshold: null,
-                    color: '#52459ea8',
-                    fillColor: {
-                        linearGradient: [0, 0, 0, 450],
-                        stops: [
-                            [0, '#79a6ff90'],
-                            [1, '#000000']
-                        ]
-                    }
-                }
-            },
-        
-        
-            tooltip: {
-                backgroundColor: '#212020',
-                style: {
-                    color: '#ffffff'
                 }
             }
-        });
-        
-        // Configuring the chart.
-        Highcharts.stockChart('teste-grafico', {
-        
-            title: {
-                text: 'Grafico Mensal',
-                align: 'left',
-            },
-        
-            layouts: {
-                padding:{
-                    left: 20
+        },
+    
+        title: {
+            style: {
+                color: '#ABA9D9',
+                fontSize: '2em'
+            }
+        },
+    
+        xAxis: {
+            type: 'datetime', // Define o eixo X como datas
+            labels: {
+                style: {
+                    color: '#79a6ff'
                 }
             },
-        
-            xAxis: {
-                tickLength: 0,
-                lineWidth: 0,
-                crosshair: {
-                    width: 1,
-                    color: '#ABA9D9',
-                    zIndex: 3
+            dateTimeLabelFormats: {
+                day: '%e de %b' // Formato do rótulo do eixo X (ex: 1 de Jan)
+            }
+        },
+    
+        yAxis: {
+            labels: {
+                style: {
+                    color: '#79a6ff'
                 }
+            }
+        },
+    
+        tooltip: {
+            backgroundColor: '#212020',
+            style: {
+                color: '#ffffff'
             },
-        
-            scrollbar: {
-                barBorderRadius: 4,
-                height: 8,
-                margin: 0,
-                trackBorderRadius: 4
+            xDateFormat: '%e de %B' // Formato da data no tooltip
+        }
+    });
+    
+    // Configuração do gráfico
+    Highcharts.stockChart('teste-grafico', {
+        title: {
+            text: 'Gráfico dos Últimos 30 Dias',
+            align: 'left',
+        },
+    
+        xAxis: {
+            tickLength: 0,
+            lineWidth: 0,
+            crosshair: {
+                width: 1,
+                color: '#ABA9D9',
+                zIndex: 3
+            }
+        },
+    
+        yAxis: {
+            gridLineWidth: 0,
+            offset: 30,
+            accessibility: {
+                description: 'Valores Diários'
+            }
+        },
+    
+        rangeSelector: {
+            buttonPosition: {
+                align: 'right'
             },
-        
-            yAxis: {
-                gridLineWidth: 0,
-                offset: 30,
-                accessibility: {
-                    description: 'price in Ethereum'
-                }
-            },
-        
-            navigator: {
-                enabled: false,
-                xAxis: {
-                    gridLineWidth: 0
-                },
-                outlineWidth: 0,
-                handles: {
-                    lineWidth: 0
-                }
-            },
-        
-            rangeSelector: {
-                buttonPosition: {
-                    align: 'right'
-                },
-                buttonSpacing: 10,
-                inputEnabled: false,
-                selected: 1
-            },
-        
-            data: {
-                // 'Jan': meses2024.api[0].VALOR.toFixed(2), 
-                // 'Fer': meses2024.api[1].VALOR.toFixed(2),
-                // 'Mar': ano2024.api[2].VALOR.toFixed(2),
-                // 'Abr': ano2024.api[3].VALOR.toFixed(2),
-                // 'Mai': ano2024.api[4].VALOR.toFixed(2),
-                // 'Jun': ano2024.api[5].VALOR.toFixed(2),
-                // 'Jul': ano2024.api[6].VALOR,
-                // 'Ago': ano2024.api[7].VALOR,
-                // 'Set': ano2024.api[8].VALOR,
-                // 'Out': ano2024.api[9].VALOR,
-                // 'Nov': ano2024.api[10].VALOR,
-                // 'Dez': ano2024.api[11].VALOR, 
-                // csvURL: 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@1e5fcf4/samples/data/btc-eth.csv',
-                // firstRowAsNames: false,
-                // startRow: 1
-            },
-        
+            buttonSpacing: 10,
+            inputEnabled: false,
+            selected: 1
+        },
+    
+        series: [{
+            name: 'Valores Diários',
+            type: 'area',
+            data: diasDoMes.map((dia, index) => [
+                Date.UTC(2024, 0, index + 1), // Gera uma data para cada dia do mês de Janeiro de 2024
+                dia.VALOR // Valor do dia vindo da API
+            ]),
             tooltip: {
-                shape: 'rect',
-                shadow: true,
-                borderWidth: 0
-            },
-        
-            series: [{
-                name: 'Valores Mensais',
-                type: 'area',
-                data: meses2024.api.map(mes => mes.VALOR), // Dados da API
-                tooltip: {
-                    valueDecimals: 2,
-                    pointFormat: '{point.y}'
-                }
-            }]
-        });
+                valueDecimals: 2,
+                pointFormat: '{point.y}'
+            }
+        }]
+    });
 }
-
 
 async function graficoMensal() {
-    const meses2024 = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=eru1BoZCMDOOEMhGBPrJjMU_LPFReYMzrFTKqf91hYWA-KOTSM_N4R6ZEC_Zlm9OeNTXmj3jg6ek3T6QQeIbQcqsWLi4DpPpm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFVXWXR0nSELR97zGgQBms-7dZnT2diY5rajuaka_Z0rKkB6hHYVkl7PctLAiJohTZfdx3kKMqce8bLhRpGbczYtqRaF3dzGGA&lib=M8G5hm_VlBnB5nuEPbx8Vg6frgnVbBec2').then(Response => Response.json());
-    console.log(meses2024); // Exiba a estrutura para verificar
-    testando(meses2024);
+    // Obtém os dados da API
+    const diasDoMes = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=eru1BoZCMDOOEMhGBPrJjMU_LPFReYMzrFTKqf91hYWA-KOTSM_N4R6ZEC_Zlm9OeNTXmj3jg6ek3T6QQeIbQcqsWLi4DpPpm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFVXWXR0nSELR97zGgQBms-7dZnT2diY5rajuaka_Z0rKkB6hHYVkl7PctLAiJohTZfdx3kKMqce8bLhRpGbczYtqRaF3dzGGA&lib=M8G5hm_VlBnB5nuEPbx8Vg6frgnVbBec2')
+        .then(response => response.json());
+
+    console.log(diasDoMes); // Exibe a estrutura para verificar
+    testando(diasDoMes);
 }
-// testando();
+
 graficoMensal();
