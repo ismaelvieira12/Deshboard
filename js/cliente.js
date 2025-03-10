72a3a3a1f4e7b624f163236b63a0deb18bc07e66
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-03-10 00:14:50 -0300

    Update

--- js/cliente.js
+++ js/cliente.js
@@ -1,26 +1,6 @@
-//Função para pegar TOTAL DE CLIENTES
-async function getTotalClients(token) {
-    const response = await fetch('https://api.beesweb.com.br/adm/customers', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        }
-    });
 
-    const data = await response.json();
-
-    if (response.ok) {
-        const totalCliente = data.statistics
-        console.log('teste', totalCliente);
-        return totalCliente;
-    } else {
-        throw new Error('Erro ao buscar clientes: ' + data.message);
-    }
-
-}
-
-
+// const token = data.api_token
+// await getTotalClients(token);
 
 // statistics: {
     // active: 73
@@ -30,50 +10,3 @@ async function getTotalClients(token) {
     // total: 89
 //}
 
-async function graficMetas(totalCliente){
-    const faltaMeta = 100 - totalCliente.active;
-    console.log(faltaMeta);
-    document.getElementById('meta').innerText = `${faltaMeta}`
-    const metas = document.querySelector('#metas');
-    new Chart(metas, {
-        type: 'pie',
-        data: {
-            labels: ['Ativados', 'Desativados'],
-            datasets: [{
-            label: 'Clientes',
-            data:[totalCliente.active, totalCliente.disable],
-            backgroundColor: [
-                '#283845', 
-                '#b8b08d',
-            ],
-            }],
-        },
-        options: {
-            responsive: true,
-            plugins: {
-            legend: {
-                position: 'top',
-                align:'center',
-                labels:{
-                usePointStyle: true,
-                textAlign: 'center',
-                useBorderRadius: true
-                }
-            },
-            title: {
-                display: true,
-                text: 'Metas',
-                font: {
-                size:20,
-                },
-            },
-            },
-            layout: {
-            padding: {
-                left: 30
-            },
-            },
-        },
-    
-    });
-}
--- js/testeApi.js
+++ js/testeApi.js
@@ -18,14 +18,12 @@ async function getAuthToken() {
     
     if (response.ok) {
         const token = data.api_token
-        await getTotalClients(token);
+        getTotalClients(token)
         return data.api_token; // Retorna o token
     } else {
         throw new Error('Erro ao fazer login: ' + data.message);
     }
 }
-
-
 
 // Função para buscar uma única página de dados
 async function fetchPage(token, page) {
@@ -376,3 +374,73 @@ setInterval(() => {
 
 
 
+//Função para pegar TOTAL DE CLIENTES
+async function getTotalClients(token) {
+    const response = await fetch('https://api.beesweb.com.br/adm/customers', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        const totalCliente = data.statistics;
+        graficMetas(totalCliente);
+        console.log('teste', totalCliente);
+        return totalCliente;
+    } else {
+        throw new Error('Erro ao buscar clientes: ' + data.message);
+    }
+
+}
+
+function graficMetas(totalCliente){
+    const faltaMeta = 100 - totalCliente.active;
+    console.log(faltaMeta);
+    document.getElementById('meta').innerText = `${faltaMeta}`
+    const metas = document.querySelector('#metas');
+    new Chart(metas, {
+        type: 'pie',
+        data: {
+            labels: ['Ativados', 'Desativados'],
+            datasets: [{
+            label: 'Clientes',
+            data:[totalCliente.active, totalCliente.disable],
+            backgroundColor: [
+                '#283845', 
+                '#b8b08d',
+            ],
+            }],
+        },
+        options: {
+            responsive: true,
+            plugins: {
+            legend: {
+                position: 'top',
+                align:'center',
+                labels:{
+                usePointStyle: true,
+                textAlign: 'center',
+                useBorderRadius: true
+                }
+            },
+            title: {
+                display: true,
+                text: 'Metas',
+                font: {
+                size:20,
+                },
+            },
+            },
+            layout: {
+            padding: {
+                left: 30
+            },
+            },
+        },
+    
+    });
+}
