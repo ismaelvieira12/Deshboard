4442fb5e5bfd9f671be0b4e2f016838d6a84d25f
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-03-27 18:02:27 -0300

    Update

---
+++ taxas/taxas.js
@@ -0,0 +1,9 @@
+
+
+function buscarDados(){
+    const ulEl = document.getElementById('box-taxas');
+    const divCabecalho = document.createElement('div').classList.add('cabecalho');
+    ulEl.appendChild(divCabecalho);
+
+    return ()
+}
