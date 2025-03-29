2073d100e7f3f7a2f72cbabf3f37fe6e6c5b1ff9
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-03-29 15:20:04 -0300

    Update

--- taxas/taxas.js
+++ taxas/taxas.js
@@ -1,8 +1,19 @@
+const lista = document.getElementById("box-taxas");
+
 
 
 function buscarDados(){
-    const ulEl = document.getElementById('box-taxas');
-    const divCabecalho = document.createElement('div').classList.add('cabecalho');
-    ulEl.appendChild(divCabecalho);
-
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <div class="taxa-line">
+            <p class="nome">Ismael Nascimento Vieira</p>
+            <p class="valor-pago">57,20</p>
+            <p class="valor-plano">55</p>
+            <p class="valor-multa">4%</p>
+            <p class="valor-juros">00,00</p>
+            <p class="juros-dia">0,025</p>
+            <p class="valor-total">57,02</p>
+        </div>
+    `;
+    lista.appendChild(li);
 }
